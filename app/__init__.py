13ea1ce5ec2ba98f0c7a7227b6148e6d0269cc30
Cedric Seidel <cedricseidel@me.com>
2026-04-13 10:55:57 +0200

    DB grundaufbau

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+login_manager = LoginManager()
 
 db = SQLAlchemy()
 
@@ -8,7 +11,13 @@ def create_app():
     app.config['SECRET_KEY'] = 'dev-secret-key'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tempusmemoria.db'
     db.init_app(app)
+    with app.app_context():
+        from app import models
+        db.create_all()
     from app.routes import main
     app.register_blueprint(main)
+    login_manager.login_view = 'main.login'
+    login_manager.init_app(app)
+
 
     return app
--- app/models.py
+++ app/models.py
@@ -1,4 +1,28 @@
 from app import db
+from app import login_manager
 
-class User(db.Model):
+from flask_login import UserMixin
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100))
+    password = db.Column(db.String(200))
+    is_admin = db.Column(db.Boolean, default=False)
+    entries = db.relationship('TimeEntry', backref='user', lazy=True)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    entries = db.relationship('TimeEntry', backref='category', lazy=True)
+
+class TimeEntry(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    start_time = db.Column(db.DateTime)
+    end_time = db.Column(db.DateTime, nullable=True)
+    description = db.Column(db.String(300))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
