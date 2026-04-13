83661cac66823ce196788c81daee40c4382a3fc3
Cedric Seidel <cedricseidel@me.com>
2026-04-13 11:18:10 +0200

    html und frontend aufbau

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,17 @@
-from flask import Blueprint
+from app.models import User
+from flask import Blueprint, render_template, request, redirect, url_for
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
     return 'Tempus Memoria is running!'
+
+@main.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    elif request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        return f"Du hast eingegeben: {username}"
